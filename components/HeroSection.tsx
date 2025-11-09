4f86250f7f48d03fc9405d147696145db85d2849
Seif Hisham <74967136+seifhisham@users.noreply.github.com>
2025-11-09 18:10:13 +0200

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -7,7 +7,11 @@ import { HeroBackgroundSlideshow } from "@/components/HeroBackgroundSlideshow";
 export function HeroSection() {
 
   const desktopImages = ["/images/hero-1.jpg", "/images/hero-2.jpg", "/images/hero-3.jpg"];
-  const mobileImages = ["/images/mobile-1.jpg", "/images/mobile-2.jpg", "/images/mobile-3.jpg"];
+  const mobileImages = [
+    "https://oyrfzypkbrotgotrqttf.supabase.co/storage/v1/object/public/hero/hero-mobile-1.jpg",
+    "https://oyrfzypkbrotgotrqttf.supabase.co/storage/v1/object/public/hero/hero-mobile-2.jpg",
+    "https://oyrfzypkbrotgotrqttf.supabase.co/storage/v1/object/public/hero/hero-mobile-3.jpg",
+  ];
 
   return (
     <section className="relative text-white h-[80svh] md:h-[90svh]">
